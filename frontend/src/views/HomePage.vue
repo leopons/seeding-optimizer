9c932a35f550db34cae7c3c78ae6433dd6e590d9
Léo Pons <leo.pons@wanadoo.fr>
2023-04-25 21:08:12 +0100

    feat(front): homepage text + error handling

--- frontend/src/views/HomePage.vue
+++ frontend/src/views/HomePage.vue
@@ -1,6 +1,19 @@
 <template>
   <div class="page-container">
     <div class="column-container">
+      <div class="title">
+        Automatically minimize collides on your start.gg bracket!
+      </div>
+      <div class="subtitle">
+        This currently supports Double Elimination brackets from 8 to 32
+        players.
+      </div>
+      <div v-if="errorNumberPlayers" class="error">
+        Error: You gave a file with {{ errorNumberPlayers }} players.
+      </div>
+      <div v-if="unexpectedError" class="error">
+        Unexpected Error: Please ensure the file is valid.
+      </div>
       <CsvLoader
         class="csv-uploader"
         :handleData="handleData"
@@ -19,36 +32,56 @@ export default {
     const store = usePlayersStore();
     return { store };
   },
+  data() {
+    return {
+      errorNumberPlayers: null,
+      unexpectedError: false,
+    };
+  },
   components: { CsvLoader },
   methods: {
     handleData(data) {
-      // Should be already sorted but just to be sure
-      data.sort((a, b) => a['"Pool Seed"'] - b['"Pool Seed"']);
-      let players = data.map((el) => el['"Entrant"']);
-      let groups = {};
-      players.forEach((str, index) => {
-        let pipeIndex = str.indexOf("|");
-        // If the '|' character is found, extract the tag
-        let tag = null;
-        if (pipeIndex !== -1) {
-          tag = str.substr(0, pipeIndex).trim();
+      try {
+        this.errorNumberPlayers = null;
+        this.unexpectedError = false;
+        if (!('"Pool Seed"' in data[0])) {
+          this.unexpectedError = true;
+          return;
         }
-        if (tag) {
-          if (tag in groups) {
-            groups[tag].push(index);
-          } else {
-            groups[tag] = [index];
+        // Should be already sorted but just to be sure
+        data.sort((a, b) => a['"Pool Seed"'] - b['"Pool Seed"']);
+        let players = data.map((el) => el['"Entrant"']);
+        if (players.length < 8 || players.length > 32) {
+          this.errorNumberPlayers = players.length;
+          return;
+        }
+        let groups = {};
+        players.forEach((str, index) => {
+          let pipeIndex = str.indexOf("|");
+          // If the '|' character is found, extract the tag
+          let tag = null;
+          if (pipeIndex !== -1) {
+            tag = str.substr(0, pipeIndex).trim();
+          }
+          if (tag) {
+            if (tag in groups) {
+              groups[tag].push(index);
+            } else {
+              groups[tag] = [index];
+            }
+          }
+        });
+        // Remove singleton groups
+        for (let key in groups) {
+          if (groups[key].length == 1) {
+            delete groups[key];
           }
         }
-      });
-      // Remove singleton groups
-      for (let key in groups) {
-        if (groups[key].length == 1) {
-          delete groups[key];
-        }
+        this.store.initPlayersAndGroups(players, groups);
+        this.$router.push("/groups");
+      } catch {
+        this.unexpectedError = true;
       }
-      this.store.initPlayersAndGroups(players, groups);
-      this.$router.push("/groups");
     },
   },
 };
@@ -63,13 +96,26 @@ export default {
 }
 .column-container {
   width: 400px;
-  height: 300px;
   background-color: white;
   border-radius: 20px;
+  padding: 20px;
+}
+.title {
+  font-size: 1.2em;
+  font-weight: bold;
+  margin-bottom: 10px;
+}
+.subtitle {
+  font-style: italic;
+  margin-bottom: 10px;
+}
+.error {
+  font-style: italic;
+  margin-bottom: 10px;
+  color: orange;
 }
 .csv-uploader {
-  margin: 20px;
-  height: calc(100% - 40px);
+  height: 200px;
   border-radius: 20px;
 }
 </style>
